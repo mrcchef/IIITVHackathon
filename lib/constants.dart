8f56c71ae37d3b061e6be8e49df91911a8ee0d17
mrcchef <201951003@iiitvadodara.ac.in>
2020-12-12 01:12:45 +0530

    Welcome page added

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,6 @@ Color contentTextColor = Color(0xff868686);
 Color navigationColor = Color(0xFF6751B5);
 Color gradientStartColor = Color(0xFF0050AC);
 Color gradientEndColor = Color(0xFF9354B9);
+
+const kBackgroundColor = Color(0xFF202020);
+const kPrimaryColor = Color(0xFFFFBD73);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './constants.dart';
 import './home_page.dart';
 
 void main() => runApp(MyApp());
@@ -12,9 +13,103 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        // backgroundColor: kBackgroundColor,
+        // scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: TextTheme(
+          display1: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          button: TextStyle(color: kPrimaryColor),
+          headline:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        ),
       ),
-      home: HomePage(),
+      home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: kBackgroundColor),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/icon.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "HUMAN ANATOMY\n",
+                            style: Theme.of(context).textTheme.display1,
+                          ),
+                          TextSpan(
+                            text: "DISCOVER YOURSELF!!!",
+                            style: Theme.of(context).textTheme.headline,
+                          )
+                        ],
+                      ),
+                    ),
+                    FittedBox(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return HomePage();
+                            },
+                          ));
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(bottom: 25),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 26, vertical: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: kPrimaryColor,
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                "START LEARNING",
+                                style:
+                                    Theme.of(context).textTheme.button.copyWith(
+                                          color: Colors.black,
+                                        ),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
